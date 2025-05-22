67efa23120f83052397044ad95e923e295cb5643
Mrudang Majmudar <mrudang@yahoo.com>
2025-05-22 19:50:36 -0400

    Conflation using RX.Net

---
+++ ConflationDotNet/ConflationDotNet/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using ConflationDotNet;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("C# Conflation .NET");
+        Subject<KeyValuePair<string, long>> subject = new Subject<KeyValuePair<string, long>>();
+
+        // Simulate a stream of key value pairs
+        var inputStream = Observable.Interval(TimeSpan.FromMilliseconds(100))
+            .Select((l) =>
+            {
+                var data = new KeyValuePair<string, long>(
+                    "Key1",
+                    l
+                );
+                Console.WriteLine(data);
+                return data;
+            });
+
+
+
+        inputStream.Subscribe(pair =>
+        {
+            subject.OnNext(pair);
+        });
+        
+        var inputStream2 = Observable.Interval(TimeSpan.FromMilliseconds(100))
+            .Select((l) =>
+            {
+                var data = new KeyValuePair<string, long>(
+                    "Key2",
+                    l
+                );
+                Console.WriteLine(data);
+                return data;
+            });
+        
+        
+        inputStream2.Subscribe(pair =>
+        {
+            subject.OnNext(pair);
+        });
+
+        // Conflate the stream by key every 1 second
+        var conflatedStream = subject.ConflateByKey(TimeSpan.FromSeconds(1));
+
+        // Subscribe to the conflated stream and print the results
+        conflatedStream.Subscribe(pair => { Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}"); });
+
+
+        // Wait for user input before exiting
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+
+}
---
+++ ConflationDotNet/ConflationDotNet/RX.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Concurrent;
+using System.Reactive.Linq;
+
+namespace ConflationDotNet;
+
+
+public static class RxExtensions
+{
+   public static IObservable<KeyValuePair<TKey, TValue>> ConflateByKey<TKey, TValue>(
+      this IObservable<KeyValuePair<TKey, TValue>> source,
+      TimeSpan interval)
+   {
+     return Observable.Create<KeyValuePair<TKey, TValue>>(
+        observer =>
+        {
+           var buffer1 = new ConcurrentDictionary<TKey, TValue>();
+           var buffer2 = new ConcurrentDictionary<TKey, TValue>();
+           var currentBuffer = buffer1;
+
+            var subscription = source.Subscribe(pair =>
+            {
+               currentBuffer[pair.Key] = pair.Value;
+            });
+
+            var timer = Observable.Interval(interval).Subscribe(_ =>
+            {
+               // Atomically swap buffers
+               var oldBuffer = Interlocked.Exchange(ref currentBuffer, currentBuffer == buffer1 ? buffer2 : buffer1);
+
+               foreach (var pair in oldBuffer)
+               {
+                  observer.OnNext(pair);
+               }
+
+               oldBuffer.Clear();
+            });
+            
+            return () =>
+            {
+               subscription.Dispose();
+               timer.Dispose();
+            };
+
+        });
+   }
+
+}
